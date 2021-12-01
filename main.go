6485918de1fccd421c1103ee26c587f5d9a2702b
davidmukiibi <david.mukiibiq@gmail.com>
2021-12-01 18:34:34 +0300

    another change

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ var SecondEndPoint = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reques
 
     var message Message
 
-    message.Response = "Yes, Scaling funds Is The best Out There. And i'm sure about that!!!!"
+    message.Response = "Yes, Scaling funds Is The best Out There. Wanna bet?"
     message.StatusCode = 200
     jsonmessage, _ := json.Marshal(message)
     w.Write([]byte(jsonmessage))
